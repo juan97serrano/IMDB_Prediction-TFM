cb1db7b34cf0f9e89e7efce99d1f9c8c3335225f
juan97serrano <jjserrano97@gmail.com>
2020-02-04 13:07:26 +0100

    code with functions

--- Scraper/Img_Scraping.py
+++ Scraper/Img_Scraping.py
@@ -2,19 +2,12 @@ from pandas import DataFrame
 from selenium import webdriver
 import pandas as pd
 import os
+import csv
 
-df = pd.read_csv("../data/title.basics.tsv", sep="\t",nrows=1000)
-#df_filter = df[df["titleType"] == "movie"]
-df_filter_reset_index = df.reset_index()
-ids_film = df_filter_reset_index["tconst"].tolist()
+def extract_info(film):
 
-driver = webdriver.Chrome(executable_path="local/chromedriver")
+    film_list = []
 
-film_list = []
-
-iter_for_saving = 10
-
-for num, film in enumerate(ids_film[:50]):
     url = "https://www.imdb.com/title/" + film + "/?ref_=fn_al_tt_1"
     driver.get(url)
 
@@ -24,11 +17,32 @@ for num, film in enumerate(ids_film[:50]):
         src=img.find_element_by_xpath("./a/img").get_attribute("src")
         os.system("wget %s --no-check-certificate -O %s.png" % (src, "../data/img/"+film))
         film_list = film_list + [[film,film+".png"]]
-    
-    if num % iter_for_saving == 0:      # Guardamos el dataframe cada x iteraciones   
-        films_df = pd.DataFrame(film_list)
-        films_df.to_csv("../data/Result_Scraper_CSV/img/IDfilmsandImg_Iter_"+str(num)+".csv")
 
-films_df = pd.DataFrame(film_list)
-films_df.to_csv("../data/Result_Scraper_CSV/img/IDfilmsandImg_Finished.csv")
-driver.close()
+    return film_list
+
+def header():
+    csv_row=[]
+    head = ["CODE","file_name"]
+    csv_row.append(head)
+    return csv_row
+
+
+df = pd.read_csv("../data/title.basics.tsv", sep="\t",nrows=1000)
+#df_filter = df[df["titleType"] == "movie"]
+df_filter_reset_index = df.reset_index()
+ids_film = df_filter_reset_index["tconst"].tolist()
+
+driver = webdriver.Chrome(executable_path="local/chromedriver")
+
+iter_for_saving = 5
+row = 0
+for num, film in enumerate(ids_film[:20]):
+    with open('../data/Result_Scraper_CSV/img/IMDBimg_andlikes.csv', 'a', encoding="utf-8") as f:                
+        
+        writer = csv.writer(f,delimiter='\t')
+        if row == 0 :
+            writer.writerows(header())
+        writer.writerows(extract_info(film))
+    
+    row += 1
+

f9c7c6ad2bfefd40c248e5f70a8fdcbb57f7a896
juan97serrano <jjserrano97@gmail.com>
2020-02-25 13:40:50 +0100

    Final Img Scraper

--- Scraper/Img_Scraping.py
+++ Scraper/Img_Scraping.py
@@ -4,6 +4,10 @@ import pandas as pd
 import numpy as np
 import os
 import csv
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.common.exceptions import NoSuchElementException
+import time
+
 
 def extract_info(film):
 
@@ -11,12 +15,12 @@ def extract_info(film):
 
     url = "https://www.imdb.com/title/" + film + "/?ref_=fn_al_tt_1"
     driver.get(url)
-
+    
     img_len = len(driver.find_elements_by_class_name("poster"))
     if img_len == 1:
         img = driver.find_element_by_class_name("poster")      
         src=img.find_element_by_xpath("./a/img").get_attribute("src")
-        os.system("wget %s --no-check-certificate -O %s.png" % (src, "../data/img/"+film))
+        os.system("wget %s --no-check-certificate -O %s.png" % (src, "data/img/"+film))
         film_list = film_list + [[film,film+".png"]]
 
     return film_list
@@ -28,20 +32,29 @@ def header():
     return csv_row
 
 
-numpy_lista = np.load("../data/lista_final_ids.npy",allow_pickle=True)
+numpy_lista = np.load("lista_final_ids.npy",allow_pickle=True)
 ids_film = list(numpy_lista)
-print(ids_film)
-driver = webdriver.Chrome(executable_path="local/chromedriver")
+print(str(ids_film[:2])+"...")
+
+driver = webdriver.Firefox(executable_path="local/geckodriver")
 
 iter_for_saving = 5
 row = 0
-for num, film in enumerate(ids_film[:20]):
-    with open('../data/Result_Scraper_CSV/img/IMDBimg_andlikes.csv', 'a', encoding="utf-8") as f:                
+for num, film in enumerate(ids_film[441367:]):
+    with open('data/IMDBimg_andlikes.csv2', 'a', encoding="utf-8") as f:                
         
         writer = csv.writer(f,delimiter='\t')
         if row == 0 :
             writer.writerows(header())
-        writer.writerows(extract_info(film))
+        try:
+            info_film = extract_info(film)
+        except NoSuchElementException:
+            info_film = [film,"No Img"]
+        writer.writerows(info_film)
     
     row += 1
+    if row % 200 == 0:
+        driver.close()
+        time.sleep(1)
+        driver = webdriver.Firefox(executable_path="local/geckodriver")
 
